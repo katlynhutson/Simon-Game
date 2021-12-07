a82317459fde80bb4426073244c155cc3b9a9d64
katlynhutson <kmhutson5@gmail.com>
2021-12-07 12:08:19 -0600

    Create player objects, reference elements, event handlers, and event listeners

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 /*----- constants -----*/
+const player = {};
+const simon = {};
 /*----- app's state variables -----*/
+let simonMoves;
+let playerMoves;
+let loseState;
 /*----- cached element references -----*/
+const gameBoard = document.querySelector('#game-board');
+const howToPlayBtn = document.querySelector('#how-to-play');
+const playBtn = document.querySelector('#play');
 /*----- event listeners -----*/
+gameBoard.addEventListener('click', colorsHandler);
+howToPlayBtn.addEventListener('click', howToPlayHandler);
+playBtn.addEventListener('click', playHandler);
 /*----- functions -----*/
+function colorsHandler(event) {
+	console.log('hi');
+}
+
+function howToPlayHandler(event) {
+	console.log('yo');
+}
+
+function playHandler(event) {
+	console.log('sup');
+}
