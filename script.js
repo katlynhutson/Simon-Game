44a45337a5b4d3d2935c963875fcf236e7b657af
katlynhutson <kmhutson5@gmail.com>
2021-12-07 14:23:49 -0600

    Make the buttons 'flash' when clicked

--- script.js
+++ script.js
@@ -9,13 +9,56 @@ let loseState;
 const gameBoard = document.querySelector('#game-board');
 const howToPlayBtn = document.querySelector('#how-to-play');
 const playBtn = document.querySelector('#play');
+// elements for each button -> I will hopefully refactor them out later
+const greenBtn = document.querySelector('#green');
+const redBtn = document.querySelector('#red');
+const yellowBtn = document.querySelector('#yellow');
+const blueBtn = document.querySelector('#blue');
+
 /*----- event listeners -----*/
 gameBoard.addEventListener('click', colorsHandler);
 howToPlayBtn.addEventListener('click', howToPlayHandler);
 playBtn.addEventListener('click', playHandler);
 /*----- functions -----*/
+// handles if a color button is clicked
 function colorsHandler(event) {
-	console.log('hi');
+	if (event.target.id === 'green') {
+		// make the button 'toggle'
+		event.target.style.color = `black`;
+		// return the button to it's initial state
+		setTimeout(greenBtnToStableState, 2000);
+	}
+	if (event.target.id === 'red') {
+		event.target.style.color = `white`;
+		setTimeout(redBtnToStableState, 2000);
+	}
+	if (event.target.id === 'yellow') {
+		event.target.style.color = `black`;
+		setTimeout(yellowBtnToStableState, 2000);
+	}
+	if (event.target.id === 'blue') {
+		event.target.style.color = `white`;
+		setTimeout(blueBtnToStableState, 2000);
+	}
+}
+
+// make a function that return the color btn to its initial state
+function greenBtnToStableState() {
+	greenBtn.style.color = 'green';
+	console.log('yeah green');
+}
+
+function redBtnToStableState() {
+	redBtn.style.color = 'red';
+	console.log('yeah red');
+}
+function yellowBtnToStableState() {
+	yellowBtn.style.color = 'yellow';
+	console.log('yeah yellow');
+}
+function blueBtnToStableState() {
+	blueBtn.style.color = 'blue';
+	console.log('yeah blue');
 }
 
 function howToPlayHandler(event) {
